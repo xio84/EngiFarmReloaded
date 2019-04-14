f55738c53c31c33457c0ca91bedb7cd2ac4edc96
Pyroblazer <30342515+Pyroblazer@users.noreply.github.com>
2019-04-14 18:26:00 +0800

    Update EggProducingFarmAnimal.java

--- src/animals/EggProducingFarmAnimal.java
+++ src/animals/EggProducingFarmAnimal.java
@@ -1,4 +1,35 @@
 package animals;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class EggProducingFarmAnimal extends FarmAnimal {
+class EggProducingFarmAnimal extends MeatProducingFarmAnimal {
+	void move(){
+		int minimum = 1, maximum = 4;
+		int chooseMoveType = ThreadLocalRandom.current().nextInt(minimum, maximum + 1);
+		hungrytick -= 1;
+		if (hungrytick <= defaulthungrytick) {
+			hungry = true;
+		}
+		switch (chooseMoveType) {
+			case 1 :
+				if (getCurrentPosition().getX()-1 > 0) {
+					currentPosition.setX(currentPosition.getX()-1);
+				}
+				break;
+			case 2 :
+				if (getCurrentPosition().getX()+1 < 2) {
+					currentPosition.setX(currentPosition.getX()+1);
+				}
+				break;
+			case 3 :
+				if (getCurrentPosition().getY()+1 < 2) {
+					currentPosition.setY(currentPosition.getY()+1);
+				}
+				break;
+			case 4 :
+				if (getCurrentPosition().getY()-1 > 0) {
+					currentPosition.setY(currentPosition.getY()-1);
+				}
+				break;
+		}
+	}
 }
