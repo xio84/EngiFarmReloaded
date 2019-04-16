be517d75b29e157da9652ddb3aab60b44f301c24
YangHansen <yanghansen02@gmail.com>
2019-04-16 17:56:32 +0700

    First Commit

--- src/animals/FarmAnimal.java
+++ src/animals/FarmAnimal.java
@@ -5,7 +5,7 @@ import point.Point;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
-class FarmAnimal {
+public class FarmAnimal {
 	protected boolean hungry;
 	protected int hungrytick;
 	protected final int defaulthungrytick = 5;
